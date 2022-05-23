bb6e5b77a151fcd13e0d6afca872b8967186eac3
diego <diego.cortinagarcia@gmail.com>
2022-05-23 10:11:49 +0200

    added class producer

--- src/claseThreads2/Producer.java
+++ src/claseThreads2/Producer.java
@@ -1,5 +1,28 @@
 package claseThreads2;
 
-public class Producer {
+public class Producer implements Runnable {
+	private SyncStack theStack;
+	private int num;
+	private static int counter = 1;
+
+	public Producer(SyncStack s) {
+		theStack = s;
+		num = counter++;
+	}
+
+	public void run() {
+		char c;
+
+		for (int i = 0; i < 200; i++) {
+			c = (char) (Math.random() * 26 + 'A');
+			theStack.push(c);
+			System.out.println("Producer" + num + ": " + c);
+			try {
+				Thread.sleep((int) (Math.random() * 1000));
+			} catch (InterruptedException e) {
+				// ignore it
+			}
+		}
+	} // END run method
 
 }
